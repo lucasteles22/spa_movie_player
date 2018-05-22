86fa3b185019e1f85b768248fd262781825a2282
Lucas Teles <lucasteles22@gmail.com>
2018-05-22 20:41:49 -0300

    Add Favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_22_234029) do
+ActiveRecord::Schema.define(version: 2018_05_22_234123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2018_05_22_234029) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "favoritable_type"
+    t.bigint "favoritable_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["favoritable_type", "favoritable_id"], name: "index_favorites_on_favoritable_type_and_favoritable_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "pg_search_documents", force: :cascade do |t|
@@ -62,5 +72,6 @@ ActiveRecord::Schema.define(version: 2018_05_22_234029) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "users"
   add_foreign_key "reviews", "users"
 end
