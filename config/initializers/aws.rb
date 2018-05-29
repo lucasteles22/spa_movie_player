79975e649aefcf791b0ecc8e705c7b0d757860cb
Lucas Teles <lucasteles22@gmail.com>
2018-05-29 13:02:50 -0300

    Fix aws credentials

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,7 @@
 Aws.config.update({
   region: "sa-east-1",
-  credentials: Aws::Credentials.new(Rails.application.credentials.aws_key,
-                                    Rails.application.credentials.aws_secret)
+  credentials: Aws::Credentials.new(Rails.application.credentials.aws[:access_key_id],
+                                    Rails.application.credentials.aws[:secret_access_key]),
 })
 
 AWS_BUCKET = Aws::S3::Resource.new.bucket("spaplayer")
